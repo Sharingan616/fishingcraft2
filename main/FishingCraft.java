8e38d249cd5d090c5c957ef1e846eecb6331e856
Benjamin <bdtovarp@gmail.com>
2016-07-29 00:48:20 -0700

    Fishing Craft 2 Initial Commit
    The start of something new.

---
+++ main/FishingCraft.java
@@ -0,0 +1,18 @@
+package fishingcraft2.main;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = FishingCraft.MODID, version = FishingCraft.VERSION)
+public class FishingCraft
+{
+    public static final String MODID = "fc2";
+    public static final String VERSION = "2.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
