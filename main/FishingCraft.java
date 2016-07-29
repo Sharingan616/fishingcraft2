f7e8d15ae7d831e20a570ce9d60f2020ea591311
Benjamin <bdtovarp@gmail.com>
2016-07-29 00:59:06 -0700

    Added creative tab

--- main/FishingCraft.java
+++ main/FishingCraft.java
@@ -3,12 +3,32 @@ package fishingcraft2.main;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 
 @Mod(modid = FishingCraft.MODID, version = FishingCraft.VERSION)
+
+/**
+ * Fishing Craft adds multiple fish, blocks, and other items into the Minecraft world,
+ * as well as redesigns the fishing functionality.
+ * 
+ * The goal of this mod is to enhance the fishing experience within the game.
+ * 
+ * @author Sharingan616 (Benjamin Tovar-Prince)
+ * @version 2.0
+ */
 public class FishingCraft
 {
     public static final String MODID = "fc2";
     public static final String VERSION = "2.0";
+    
+    public static CreativeTabs fcTab = new CreativeTabs("fishingcraft_tab")
+    {
+		public Item getTabIconItem() {
+			return Items.fishing_rod;
+		}
+    };
     
     @EventHandler
     public void init(FMLInitializationEvent event)
