605feb8ffe13270534a1117e7bd9ec8544996ce5
Benjamin <bdtovarp@gmail.com>
2016-07-30 19:12:58 -0700

    Added rods and meat to the item list

--- common/items/FCItems.java
+++ common/items/FCItems.java
@@ -2,6 +2,9 @@ package fishingcraft2.common.items;
 
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import fishingcraft2.common.items.fish.ItemFish;
+import fishingcraft2.common.items.fish.ItemFishMeat;
+import fishingcraft2.common.items.rods.ItemIronFishingRod;
+import fishingcraft2.common.items.rods.ItemWoodenFishingRod;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -33,5 +36,22 @@ public class FCItems
     public static final Item fishTench = (new ItemFish(2, "Tench")).setPotionEffect(Potion.hunger.id, 30, 0, 0.8F);
     public static final Item fishMackerel = (new ItemFish(2, "Mackerel")).setPotionEffect(Potion.hunger.id, 30, 0, 0.8F);
 
+ 	//Meat
+    public static final Item rawFishMorsel = new ItemFishMeat(1, "Fish_MorselRaw").setPotionEffect(Potion.hunger.id, 30, 0, 0.8F);
+    public static final Item cookedFishMorsel = (new ItemFishMeat(2, "Fish_MorselCooked"));
+    
+    //Rods
+    public static final Item woodenFishingRod = (new ItemWoodenFishingRod("Wooden_Fishing_Rod#B"));
+    public static final Item woodenFishingRodBG = (new ItemWoodenFishingRod("Baited_Wooden_Fishing_Rod-blueGill", fishBlueGill));
+    public static final Item woodenFishingRodFrogEggs = (new ItemWoodenFishingRod("Baited_Wooden_Fishing_Rod-frogEggs", frogeggs));
+    public static final Item woodenFishingRodMorsel = (new ItemWoodenFishingRod("Baited_Wooden_Fishing_Rod-morsel", rawFishMorsel));
+    public static final Item woodenFishingRodSP = (new ItemWoodenFishingRod("Baited_Wooden_Fishing_Rod-srrbPiranha", fishSmallPiranha));
+    public static final Item woodenFishingRodWorm = (new ItemWoodenFishingRod("Baited_Wooden_Fishing_Rod-worm", worm));
+    public static final Item ironFishingRod = (new ItemIronFishingRod("Iron_Fishing_Rod#B"));
+    public static final Item ironFishingRodBG = (new ItemIronFishingRod("Baited_Iron_Fishing_Rod-blueGill", fishBlueGill));
+    public static final Item ironFishingRodFrogEggs = (new ItemIronFishingRod("Baited_Iron_Fishing_Rod-frogEggs", frogeggs));
+    public static final Item ironFishingRodMorsel = (new ItemIronFishingRod("Baited_Iron_Fishing_Rod-morsel", rawFishMorsel));
+    public static final Item ironFishingRodSP = (new ItemIronFishingRod("Baited_Iron_Fishing_Rod-srrbPiranha", fishSmallPiranha));
+    public static final Item ironFishingRodWorm = (new ItemIronFishingRod("Baited_Iron_Fishing_Rod-worm", worm));  
 
 }
